b040e7169404e01814158ab80a1828270da2f627
Alejandro <bolivaral@gmail.com>
2018-04-24 20:02:35 -0400

    Adding new files

--- 04_code.ps1
+++ 04_code.ps1
@@ -1,4 +1,4 @@
-﻿###################################################################
+###################################################################
 # Chapter 4 - Code File
 ###################################################################
 
@@ -12,7 +12,7 @@ Function create-password {
     $password = ""
 
     # For numbers between 33 and 126
-    For ($a=33;$a –le 126;$a++) {
+    For ($a=33;$a �le 126;$a++) {
         # Add the Ascii text for the ascii number referenced. 
         $ascii += ,[char][byte]$a
     }
@@ -24,11 +24,10 @@ Function create-password {
     return $password
 }
 # Create four 30 character passwords
-create-password
-create-password
-create-password
-create-password
-
+#create-password
+#create-password
+#create-password
+#create-password
 
 ###################################################################
 # Example On How To Load the .NET Assemblies
@@ -37,7 +36,7 @@ create-password
 Write-host "Loading the .NET System.Security Assembly For Encryption"
 Add-Type -AssemblyName System.Security -ErrorAction SilentlyContinue -ErrorVariable err
 if ($err) {
-    Write-host "Error Importing the .NET System.Security Assembly."
+Write-host "Error Importing the .NET System.Security Assembly."
     PAUSE
     EXIT
 }
@@ -85,6 +84,7 @@ function Encrypt-String { param($String, $Pass, $salt="CreateAUniqueSalt", $init
 }
 Encrypt-String "Encrypt This String" "A_Complex_Password_With_A_Lot_Of_Characters"
 
+
 ###################################################################
 # Example On How To Decrypt a String
 ###################################################################
@@ -122,50 +122,3 @@ function Decrypt-String { param($Encrypted, $pass, $salt="CreateAUniqueSalt", $i
    }
 }
 Decrypt-String "hK7GHaDD1FxknHu03TYAPxbFAAZeJ6KTSHlnSCPpJ7c=" "A_Complex_Password_With_A_Lot_Of_Characters"
-
-
-###################################################################
-# Example On How To Encode a Password
-###################################################################
-$pass = "A_Complex_Password_With_A_Lot_Of_Characters"
-$encodedpass = [System.Text.Encoding]::Unicode.GetBytes($pass)
-$encodedvalue = [Convert]::ToBase64String($encodedpass)
-$encodedvalue
-
-###################################################################
-# Example On How To decode a Password
-###################################################################
-$encvalue = "QQBfAEMAbwBtAHAAbABlAHgAXwBQAGEAcwBzAHcAbwByAGQAXwBXAGkAdABoAF8AQQBfAEwAbwB0AF8ATwBmAF8AQwBoAGEAcgBhAGMAdABlAHIAcwA="
-$encbytes = [System.Convert]::FromBase64String($encvalue)
-$decodedvalue = [System.Text.Encoding]::Unicode.GetString($encbytes)
-$decodedvalue
-
-############################################################################
-# Full example on how to properly decode a string for use in decrypting data
-############################################################################
-# Cheat Sheet for Script example.
-$xmlfile = "c:\temp\Example.xml"
-$RTD = "mAF8AQwBoAGEAcgBhAGMAdABlAHIAcwA="
-
-# The script is invoked with the command line of:
-# powershell.exe -file "c:\temp\Example.ps1" "c:\temp\Example.xml" "mAF8AQwBoAGEAcgBhAGMAdABlAHIAcwA=" 
-param($xmlfile, $RTD)
-
-# Read the XML file for the Answer File Decryptor
-[xml] $xml = (get-content $xmlfile)
-$AFD = $xml.GetElementsByTagName("AFD").Name
-
-# Define the Script Side Decryptor
-$SSD = "QQBfAEMAbwBtAHAAbABlAHgAXwBQAGEAc"
-
-# Combine the Decryptors
-$encvalue = $SSD + $AFD + $RTD 
-# Decode the values
-$encbytes = [System.Convert]::FromBase64String($encvalue)
-$decrypt = [System.Text.Encoding]::Unicode.GetString($encbytes)
-
-# Decrypt the string.
-Decrypt-String "hK7GHaDD1FxknHu03TYAPxbFAAZeJ6KTSHlnSCPpJ7c=" $decrypt
-
-PAUSE
-
